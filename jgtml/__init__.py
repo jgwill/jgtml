e239a263e08d1c2777dbd20272bf75ca694c1911
JGWill <jgi@jgwill.com>
2024-02-23 12:23:31 -0500

    migrating:pov_target_calculation_n_output240223

--- jgtml/__init__.py
+++ jgtml/__init__.py
@@ -5,6 +5,6 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-from calc import calculate_target_variable_min_max as calc_targets
+from calc import calculate_target_variable_min_max as _calc_targets,pov_target_calculation_n_output240223 as pov_calc_targets 
 
 #from jgtpy import JGTPDS as pds,JGTADS as ads,JGTPDSP as pds
--- jgtml/calc.py
+++ jgtml/calc.py
@@ -76,3 +76,67 @@ def calculate_target_variable_min_max(dfsrc, crop_last_dt=None, crop_start_dt=No
     return df
 
 
+
+
+def pov_target_calculation_n_output240223(indir_cds, outdir_tmx, crop_start_dt, crop_end_dt, i, t):
+    ifn = i.replace('/','-')
+        #read instrument prop
+    iprop=pds.get_instrument_properties(i)
+    pipsize = iprop['pipsize']
+    nb_decimal = len(str(pipsize).split('.')[1]) if '.' in str(pipsize) else len(str(pipsize))
+    rounder = nb_decimal
+
+        # Possible crop of the dataframe to a specific date range
+
+        # Read the source data of already calculated CDS
+    df_cds_source = pd.read_csv(f'{indir_cds}/{ifn}_{t}.csv', index_col=0, parse_dates=True)
+
+    df_result_tmx = calculate_target_variable_min_max(df_cds_source, crop_end_dt, crop_start_dt, rounder=rounder, pipsize=pipsize)
+
+        # Save the result to a csv file
+    output_all_cols_fn = f'{outdir_tmx}/{ifn}_{t}.csv'
+    try:
+        df_result_tmx.to_csv(output_all_cols_fn, index=True)
+        print(f"Saved to {output_all_cols_fn}")
+    except Exception as e:
+        print(f"Error occurred while saving to {output_all_cols_fn}: {str(e)}")
+
+    keeping_columns = ['Low', 'fdbs', 'fdbb', 'tmax', 'tmin', 'p', 'l', 'target']
+    sel = df_result_tmx[keeping_columns].copy()
+    sel = sel[(sel['p'] != 0) | (sel['l'] != 0)]
+    
+    output_sel_cols_fn = f'{outdir_tmx}/{ifn}_{t}_sel.csv'
+    try:
+        sel.to_csv(output_sel_cols_fn, index=True)
+        print(f"Saved to {output_sel_cols_fn}")
+    except Exception as e:
+        print(f"Error occurred while saving to {output_sel_cols_fn}: {str(e)}")
+
+    keeping_columns_sel2 = ['Open', 'High', 'Low', 'Close', 'fdbs', 'fdbb', 'target']
+    df_selection2 = df_result_tmx[keeping_columns_sel2].copy()
+    df_selection2['target'] = df_selection2['target'].round(rounder)
+    df_selection2 = df_selection2[(df_selection2['target'] != 0)]
+    
+    output_tnd_targetNdata_fn = f'{outdir_tmx}/{ifn}_{t}_tnd.csv'
+    try:
+        df_selection2.to_csv(output_tnd_targetNdata_fn, index=True)
+        print(f"Saved to {output_tnd_targetNdata_fn}")
+    except Exception as e:
+        print(f"Error occurred while saving to {output_tnd_targetNdata_fn}: {str(e)}")
+    
+    reporting(df_selection2, ifn, t, pipsize,tlid_tag)
+
+
+def reporting(df_selection2, ifn, t, pipsize,tlid_tag):
+
+    report_file = f"{default_jgtpy_data_full}/report-calc-{tlid_tag}.txt"
+    print("Reporting to:", report_file)
+    print(" tail -f ", report_file)
+    
+    with open(report_file, "a") as f:
+        f.write(f"--- {ifn}_{t} ---\n")
+        f.write(f"Sum of target: {df_selection2['target'].sum()}\n\n")
+        #f.write(f" (rounded): {(df_selection2['target'].sum()).round(2)}\n\n")
+    
+
+
