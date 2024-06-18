db4d6d4018de434c808f75680eeed7622860a745
JGWill <jgi@jgwill.com>
2024-06-18 15:06:35 -0400

    refactoring

---
+++ jgtml/ptottf.py
@@ -0,0 +1,5 @@
+
+from jgtpy import JGTCDSSvc as svc
+from jgtutils import jgtpov as jpov
+from jgtutils.jgtconstants import (MFI_VAL, ZCOL, AO)
+
--- jgtml_cli-ttf-proto-01-2406181221.py
+++ jgtml_cli-ttf-proto-01-2406181221.py
@@ -40,11 +40,15 @@ def make_htf_created_columns_array(workset,t):
             created_columns.append(new_col_name)
     return created_columns
 
-def create_ttf_csv(i, t, use_full=False, use_fresh=True, quotescount=333):
+def read_ttf_csv(i, t, use_full=False):
+    output_filename=get_outfile_fullpath(i,t,use_full)
+    return pd.read_csv(output_filename, index_col=0)
+  
+def create_ttf_csv(i, t, use_full=False, use_fresh=True, quotescount=333,force_read=False):
     povs = jpov.get_higher_tf_array(t)
     ttf = pd.DataFrame()
 
-    workset = svc.get_higher_cdf_datasets(i, t, use_full=use_full, use_fresh=use_fresh, quotescount=quotescount, quiet=True)
+    workset = svc.get_higher_cdf_datasets(i, t, use_full=use_full, use_fresh=use_fresh, quotescount=quotescount, quiet=True, force_read=force_read)
     ttf=workset[t]
     created_columns = make_htf_created_columns_array(workset, t)
     
@@ -86,7 +90,8 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--timeframe", required=True, help="Timeframe (e.g., D1, H4)")
     parser.add_argument("-uf", "--full", action="store_true", help="Use full dataset")
     parser.add_argument("-new", "--fresh", action="store_true", help="Use fresh data")
+    parser.add_argument("-fr", "--force_read", action="store_true", help="Force to read CDS (should increase speed but relies on existing data)")
     parser.add_argument("-c", "--quotescount", type=int, default=333, help="Number of quotes to retrieve (default: 333)")
 
     args = parser.parse_args()
-    create_ttf_csv(args.instrument, args.timeframe, args.full, args.fresh, args.quotescount)
+    create_ttf_csv(args.instrument, args.timeframe, args.full, args.fresh, args.quotescount, args.force_read)
