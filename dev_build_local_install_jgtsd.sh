4049f6238c6489bf764bbf1e8b6be8c2ddc8664b
JGWill <jgi@jgwill.com>
2024-07-10 23:53:41 -0400

    stuff

--- dev_build_local_install_jgtsd.sh
+++ dev_build_local_install_jgtsd.sh
@@ -1,2 +1,2 @@
-v=80
-. pre-build.sh ;make dist;(cdjgtd;conda activate jgtsd;pip uninstall jgtml -y;pip install /a/src/_jgt/jgtml/dist/jgtml-0.0.$v-py3-none-any.whl)
+v=93
+. pre-build.sh  && make dist && (cdjgtd;conda activate jgtsd;pip uninstall jgtml -y;pip install /a/src/_jgt/jgtml/dist/jgtml-0.0.$v-py3-none-any.whl)
