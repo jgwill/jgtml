b5870af3926fd6bb3ce28336f1ee5dd104e210a2
JGWill <jgi@jgwill.com>
2024-07-24 19:18:07 -0400

    pto:Updated jgtapp.py

--- jgtml/jgtapp.py
+++ jgtml/jgtapp.py
@@ -24,7 +24,7 @@
 
 
 
-# Wed 24 Jul 2024 07:09:02 PM EDT
+# Wed 24 Jul 2024 07:18:07 PM EDT
 # SOURCE NAME: /b/Dropbox/jgt/drop/fnml.py
 ########################
  
@@ -46,14 +46,21 @@ def ocds(instrument, timeframe):
   subprocess.run(['jgtcli', '-i', instrument, '-t', timeframe, '--full', '-mfi', '-ba', '-ta', '-old'])
 
   
-def ttf(instrument, timeframe,pn="ttf",use_fresh=False):
+def ttf(instrument, timeframe,pn="ttf",clh=None,use_fresh=False):
   use_fresh_arg = '-old' if not use_fresh else '--fresh'
-  subprocess.run(['ttfcli', '-i', instrument, '-t', timeframe, '--full', use_fresh_arg, '-pn', pn])
+  ttf_args = ['ttfcli', '-i', instrument, '-t', timeframe, '--full', use_fresh_arg, '-pn', pn]
+  if clh is not None:
+    ttf_args.extend(['-clh', clh])
+  print("TTF is being ran by jgtapp with args: ", ttf_args)
+  subprocess.run(ttf_args)
 
 
 def mlf(instrument, timeframe,pn="ttf",clh=None,total_lagging_periods=5,use_fresh=False):
   use_fresh_arg = '-old' if not use_fresh else '--fresh'
-  subprocess.run(['mlfcli', '-i', instrument, '-t', timeframe, '--full', use_fresh_arg, '-pn', pn,'--total_lagging_periods',total_lagging_periods])
+  mlf_args = ['mlfcli', '-i', instrument, '-t', timeframe, '--full', use_fresh_arg, '-pn', pn,'--total_lagging_periods',total_lagging_periods]
+  if clh is not None:
+    mlf_args.extend(['-clh', clh])
+  subprocess.run(mlf_args)
 
   
 
@@ -110,7 +117,7 @@ def main():
   parser_prep_ttf_10.add_argument('-i','--instrument', help='Instrument symbol')
   parser_prep_ttf_10.add_argument('-t','--timeframe', help='Timeframe')
   parser_prep_ttf_10.add_argument('-pn','--patternname', help='Pattern Name')
-  parser_prep_ttf_10.add_argument('-clh','--columns_list_from_higher_tf', help='Columns List')
+  parser_prep_ttf_10.add_argument('-clh','--columns_list_from_higher_tf', help='Columns List', default=None)
   parser_prep_ttf_10.add_argument('-new','--fresh', action='store_true', help='Use the fresh data')
 
 
@@ -119,7 +126,7 @@ def main():
   parser_prep_mlf_22.add_argument('-i','--instrument', help='Instrument symbol')
   parser_prep_mlf_22.add_argument('-t','--timeframe', help='Timeframe')
   parser_prep_mlf_22.add_argument('-pn','--patternname', help='Pattern Name')
-  parser_prep_mlf_22.add_argument('-clh','--columns_list_from_higher_tf', help='Columns List')
+  parser_prep_mlf_22.add_argument('-clh','--columns_list_from_higher_tf', help='Columns List', default=None)
   parser_prep_mlf_22.add_argument('-tlp','--total_lagging_periods', help='Total Lagging Periods')
   parser_prep_mlf_22.add_argument('-new','--fresh', action='store_true', help='Use the fresh data')
   
@@ -152,9 +159,9 @@ def main():
   elif args.command == 'ocds':
     ocds(args.instrument, args.timeframe)
   elif args.command == 'ttf':
-    ttf(args.instrument, args.timeframe,args.patternname,args.fresh)
+    ttf(args.instrument, args.timeframe,args.patternname,args.columns_list_from_higher_tf,args.fresh)
   elif args.command == 'mlf':
-    mlf(args.instrument, args.timeframe,args.patternname,args.total_lagging_periods,args.fresh)
+    mlf(args.instrument, args.timeframe,args.patternname,args.columns_list_from_higher_tf,args.total_lagging_periods,args.fresh)
   elif args.command == 'ttfmxwf':
     ttfmxwf(args.instrument, args.fresh)
   elif args.command == 'mx':
