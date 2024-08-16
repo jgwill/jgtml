6289ddcae9440b913a1c080d8b7c1b0439785213
JGWill <jgi@jgwill.com>
2024-08-15 23:37:10 -0400

    add pre-dist-fdbscan

---
+++ pre-dist-fdb_scan.sh
@@ -0,0 +1,17 @@
+sdir="/b/Dropbox/jgt/drop/LLM_Refactoring__2406181109/src/jgtml_pto_tide_alligator"
+for f in JGTBalanceAnalyzer.py SignalOrderingHelper.py JGTOutputHelper.py fdb_scanner_2408.py;do
+  if [ -e jgtml/$f ]; then
+
+  echo "Exist already $f, commit in case"
+  git add jgtml/$f &>/dev/null
+  git commit jgtml/$f -m "pto:Aucas $f" &>/dev/null
+
+  fi
+  cp $sdir/$f jgtml/$f
+  chmod 444 jgtml/$f #lock the file back 
+  git add jgtml/$f &>/dev/null
+  git commit jgtml/$f -m "pto:Updated $f" &>/dev/null
+done
+
+
+
