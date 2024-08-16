17209b5131ce2b9bb09cbd011c6cce08ad9c3934
JGWill <jgi@jgwill.com>
2024-08-15 20:10:15 -0400

    chg:makefile build

--- release-n-test.sh
+++ release-n-test.sh
@@ -1,8 +1,11 @@
 git commit package.json pyproject.toml jgtml/__init__.py -m bump &>/dev/null
 . bump_jgtpy.sh
 #. pre-build.sh
-
+sleep 2
 pip install -U jgtpy jgtutils
+sleep 1
+make make bump_jgtpy
+
 . scripts/version-patcher.sh
 cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
 git commit . -m "v$cversion" && git tag "$cversion" && git push --tags && git push 
