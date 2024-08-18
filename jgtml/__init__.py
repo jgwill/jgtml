23966cfaef9926dc86b97f5e8529c1285d65965f
JGWill <jgi@jgwill.com>
2024-08-17 23:56:36 -0400

    bump:version

--- jgtml/__init__.py
+++ jgtml/__init__.py
@@ -1,10 +1,10 @@
 # jgtml
-version='0.0.196'
+version='0.0.197'
 import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-import mfihelper2 as mfih
+
 
 from jtc import (
     calculate_target_variable_min_max as calc_target_from_df,
@@ -14,12 +14,12 @@ from jtc import (
 
 from jplt import (an_biv_plt2ds as plot_an_biv_plt2ds, an_bivariate_plot00 as plot_an_bivariate_plot00)
 
-import jdash as jdb
-
-# from jgtpy import JGTPDS as pds,JGTADS as ads,JGTPDSP as pds
+from ptottf import create_ttf_csv as create_ttf
+from mlfsvc import create_mlf
 
 def __init__():
     """
     Initialize the jgtml module.
     """
+    print("jgtml version: ", version)
     pass
