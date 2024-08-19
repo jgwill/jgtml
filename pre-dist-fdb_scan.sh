c0587d7bdfecc4605544681e2410732f659318cb
JGWill <jgi@jgwill.com>
2024-08-19 05:17:49 -0400

    FDBScan and jgtapp now here

--- pre-dist-fdb_scan.sh
+++ pre-dist-fdb_scan.sh
@@ -1,17 +1,20 @@
 sdir="/b/Dropbox/jgt/drop/LLM_Refactoring__2406181109/src/jgtml_pto_tide_alligator"
-for f in JGTBalanceAnalyzer.py SignalOrderingHelper.py JGTOutputHelper.py fdb_scanner_2408.py;do
-  if [ -e jgtml/$f ]; then
+echo "FDBScan MIGRATED HERE from $sdir 240819.  JGTBalanceAnalyzer.py SignalOrderingHelper.py JGTOutputHelper.py fdb_scanner_2408.py"
 
-  echo "Exist already $f, commit in case"
-  git add jgtml/$f &>/dev/null
-  git commit jgtml/$f -m "pto:Aucas $f" &>/dev/null
 
-  fi
-  cp -f $sdir/$f jgtml/$f
-  chmod 444 jgtml/$f #lock the file back 
-  git add jgtml/$f &>/dev/null
-  git commit jgtml/$f -m "pto:Updated $f" &>/dev/null
-done
+#for f in JGTBalanceAnalyzer.py SignalOrderingHelper.py JGTOutputHelper.py fdb_scanner_2408.py;do
+#  if [ -e jgtml/$f ]; then
+#
+#  echo "Exist already $f, commit in case"
+#  git add jgtml/$f &>/dev/null
+#  git commit jgtml/$f -m "pto:Aucas $f" &>/dev/null
+#
+#  fi
+#  cp -f $sdir/$f jgtml/$f
+#  chmod 444 jgtml/$f #lock the file back 
+#  git add jgtml/$f &>/dev/null
+#  git commit jgtml/$f -m "pto:Updated $f" &>/dev/null
+#done
 
 
 
