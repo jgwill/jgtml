7f791ecdc7a5a9081a7f52b40a247375070be858
JGWill <jgi@jgwill.com>
2024-08-19 20:43:58 -0400

    chg:Enhanced support ENV TEMPLATE
    Added env var:
    * FDBSCAN_DIR
    * TEMPLATE_CHART_N_LINKS
    * TEMPLATE_UTILITES

--- jgtml/JGTOutputHelper.py
+++ jgtml/JGTOutputHelper.py
@@ -1,16 +1,9 @@
 import json
 import os
 
-def generate_markdown_from_json_file(json_filepath):
-    with open(json_filepath, 'r') as file:
-        data = json.load(file)
-    
-    signalbar = data['signalbar']
-    currentbar = data['currentbar']
-    
+def create_entry_signal_summary(data):
     markdown_output = f"""
-# Trade Signal Details
-
+# Entry Signal Details
 ## Trade Information
 - **Entry Rate:** {data['entry']}
 - **Stop Rate:** {data['stop']}
@@ -19,22 +12,20 @@ def generate_markdown_from_json_file(json_filepath):
 - **Tlid ID:** {data['tlid_id']}
 - **Instrument:** {data['i']}
 - **Timeframe:** {data['t']}
-
-![](signal.png)
-
-
-[M1](charts/M1.png)-[W1](charts/W1.png)-[D1](charts/D1.png)-[H4](charts/H4.png)-[H1](charts/H1.png)-[m15](charts/m15.png)-[m5](charts/m5.png)
-
 """
 
+    return markdown_output
+
+TEMPLATE_CHART_N_LINKS="""
+![](charts/signal.png)
+[M1](charts/M1.png)-[W1](charts/W1.png)-[D1](charts/D1.png)-[H4](charts/H4.png)-[H1](charts/H1.png)-[m15](charts/m15.png)-[m5](charts/m5.png)
+"""
+ 
 
 
-    markdown_output += """
-
+TEMPLATE_UTILITES = """
 ----
-
 ## Utilities
-
 * [entry.sh](.jgt/entry.sh)
 * [cancel.sh](.jgt/cancel.sh)
 * [watch.sh](.jgt/watch.sh)
@@ -42,13 +33,10 @@ def generate_markdown_from_json_file(json_filepath):
 * [update.sh](.jgt/update.sh)
 * [env.sh](.jgt/env.sh)
 * Other scripts might include: .jgt/mv.sh, .jgt/rmtrade.sh, .jgt/xtrail.sh, .jgt/xfdb.sh
-
 ### CLI Commands
-
-
-```bash
-. .jgt/env.sh
-fxtr -id $OrderID $demo_arg
+```sh
+#.jgt/env.sh
+#fxtr -id $OrderID $demo_arg
 #fxrmorder -id $OrderID $demo_arg
 #fxclosetrade -tid $trade_id $demo_arg
 #fxtr -id $OrderID $demo_arg
@@ -59,41 +47,56 @@ fxtr -id $OrderID $demo_arg
 #jgtapp fxmvstopfdb -tid $trade_id -i $instrument -t $timeframe  $demo_arg
 #jgtapp fxstatusorder -id $OrderID  $demo_arg
 ```
-
 #### More
-
 * run 
-
 ```sh
-jgtapp --help
+mkdir -p helps
+jgtapp --help > helps/jgtapp.txt
+fxtr --help > helps/fxtr.txt
 ```
-
-
 ### --@STCIssue Future Enhancements
-
 * CLI Commands to run, not hard coded scripts
 * Example : _fxtrupdate, _jgtsession_mksg, _jgtsession_vswsopen, _jgtsession_mkads_ctx_timeframe, _jgtsession_mkads_all_timeframes
-
 """
 
+def create_bar_to_markdown(bar,title):
+    mddata=f"""
 
-
-    markdown_output += f"""
-
-## Signal Bar Data
+## {title}
 | Metric           | Value         |
 |------------------|---------------|
 """
-    for key, value in signalbar.items():
-        markdown_output += f"| {key} | {value} |\n"
+    for key, value in bar.items():
+        mddata+=f"| {key} | {value} |\n"
+    return mddata
+        
+def get_chart_n_links():
+    if 'TEMPLATE_CHART_N_LINKS' is os.environ:
+        return os.getenv('TEMPLATE_CHART_N_LINKS')
+    return TEMPLATE_CHART_N_LINKS
 
-    markdown_output += """
-## Current Bar Data
-| Metric           | Value         |
-|------------------|---------------|
-"""
-    for key, value in currentbar.items():
-        markdown_output += f"| {key} | {value} |\n"
+def get_utilities():
+    if 'TEMPLATE_UTILITES' is os.environ:
+        return os.getenv('TEMPLATE_UTILITES')
+    return TEMPLATE_UTILITES
+
+def generate_markdown_from_json_file(json_filepath):
+    with open(json_filepath, 'r') as file:
+        data = json.load(file)
+    
+    signalbar = data['signalbar']
+    currentbar = data['currentbar']
+    
+    markdown_output = create_entry_signal_summary(data)
+
+    markdown_output += get_chart_n_links()
+
+
+    markdown_output += TEMPLATE_UTILITES
+
+    markdown_output += create_bar_to_markdown(signalbar,"Signal Bar Data")
+    
+    markdown_output += create_bar_to_markdown(currentbar,"Current Bar Data")
 
 
     return markdown_output
@@ -110,8 +113,16 @@ def serialize_signal_to_markdown_file_from_json_file(json_filepath,quiet=True):
       print(f"Error saving markdown file to {markdown_filepath} {e}")
       return None
   
-def serialize_signal_to_json_file(i,t,o,signal_bar,current_bar,quiet=True,signal_dir="data/signals",ext="json",indent=2):
-      
+FDBSCAN_DIR_DEFAULT="data/signals"
+
+def get_fdbscan_dir():
+    if 'FDBSCAN_DIR' in os.environ:
+        return os.getenv('FDBSCAN_DIR')
+    return FDBSCAN_DIR_DEFAULT
+    
+def serialize_signal_to_json_file(i,t,o,signal_bar,current_bar,quiet=True,signal_dir=None,ext="json",indent=2):
+      if signal_dir is None:
+          signal_dir=get_fdbscan_dir()
       tlid_id=o["tlid_id"]
       o["i"]=i
       o["t"]=t
@@ -129,7 +140,9 @@ def serialize_signal_to_json_file(i,t,o,signal_bar,current_bar,quiet=True,signal
         print(f"Error saving signal to {signal_savepath} {e}")
         return None
 
-def create_signal_savepath(i,t, tlid_id, ext, signal_dir="data/signals"):
+def create_signal_savepath(i,t, tlid_id, ext, signal_dir=None):
+    if signal_dir is None:
+        signal_dir=get_fdbscan_dir()
     signal_fn = create_signal_filename(i,t, tlid_id, ext)
     signal_savepath =os.path.join(signal_dir,signal_fn)
     return signal_savepath
