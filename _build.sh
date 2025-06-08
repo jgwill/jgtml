4c6baa5aae40a5550751b61a86021048f258464c
JGWill <jgi@jgwill.com>
2025-06-08 12:32:37 -0400

    enh

--- _build.sh
+++ _build.sh
@@ -1,1 +1,1 @@
-make clean;make bump_version ;make dist && twine upload dist/*
+make clean;v=$(make bump_version);make dist && twine upload dist/* && git tag $v ;git push --tags &>/dev/null && git push &>/dev/null 
