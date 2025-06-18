c41af8b0f46d7c189eab06b53c7f6f8276f7f9a2
JGWill <jgi@jgwill.com>
2025-06-18 17:41:09 -0400

    fix

--- BUILDRELEASETAG.sh
+++ BUILDRELEASETAG.sh
@@ -1,7 +1,6 @@
 make clean;v=$(make bump_version);echo $v ;make dist && twine upload dist/* && \
-git add pyproject.toml package.json jgtml/__init__.py && \
-git commit pyproject.toml package.json jgtml/__init__.py -m v$v  && \
-git tag $v
+git commit pyproject.toml package.json jgtml/__init__.py -m v$v &>/dev/null  && \
+git tag $v &>/dev/null && \
 git push &>/dev/null 
 git push --tags &>/dev/null
 
