d3b609ed8ffaa44c508d88e4b9c4c04bf6971aa9
JGWill <jgi@jgwill.com>
2025-06-18 17:44:06 -0400

    Update BUILDRELEASETAG.sh to include pip install command
    - Added `pip install -e .` to the release tagging script to ensure the package is installed in editable mode after building and tagging.
    
    This change streamlines the release process by automatically installing the latest version of the package.

--- BUILDRELEASETAG.sh
+++ BUILDRELEASETAG.sh
@@ -1,6 +1,7 @@
 make clean;v=$(make bump_version);echo $v ;make dist && twine upload dist/* && \
 git commit pyproject.toml package.json jgtml/__init__.py -m v$v &>/dev/null  && \
 git tag $v &>/dev/null && \
+pip install -e .
 git push &>/dev/null 
 git push --tags &>/dev/null
 
