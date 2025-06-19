78eb90228db178fdde92da649589544ad009dcde
JGWill <jgi@jgwill.com>
2025-06-19 16:14:31 -0400

    Add script _REFRESH_HIGH_CDS_current.sh for automated high timeframe CDS data refresh and upload
    - Implemented a new script to refresh high timeframe CDS data for multiple currency pairs and upload the results to a specified directory.
    - The script utilizes jgtcli for data retrieval and droxul for uploading, enhancing the automation of data distribution.
    
    This addition streamlines the process of maintaining up-to-date market data for trading analysis.
    
    -----
    APPENDIX
    -----
    
    jgtcli comes from package : jgtpy
    
    #52

---
+++ _REFRESH_HIGH_CDS_current.sh
@@ -0,0 +1,10 @@
+#for t in W1 M1 D1 H4 H1;do for i in EUR/USD USD/CAD SPX500 AUD/USD AUD/CAD GBP/USD;do jgtapp cds -i $i -t $t --fresh ;done;done
+#(cd $JGTPY_DATA/cds;for f in *.csv;do droxul upload $f /dist/data/current/cds;done)
+
+for t in W1 M1 D1 H4 H1;do 
+  for i in EUR/USD USD/CAD SPX500 AUD/USD AUD/CAD GBP/USD;do 
+    (jgtcli -i $i -t $t --fresh && (p=$(jgtcli -i $i -t $t --fresh -vp);droxul upload $p /dist/data/current/cds))
+  done
+done
+
+#(cd $JGTPY_DATA/cds;for f in *.csv;do droxul upload $f /dist/data/current/cds;done)
