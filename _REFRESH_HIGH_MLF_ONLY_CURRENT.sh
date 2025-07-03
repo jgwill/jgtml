f083b795091d510729edfff681047fc4f1d4a216
JGWill <jgi@jgwill.com>
2025-07-03 10:31:34 -0400

    Add new script _REFRESH_HIGH_MLF_ONLY_CURRENT.sh to refresh MLF data for current timeframes. Update _REFRESH_HIGH_MLF_ONLY.sh to include environment variable handling and maintain consistency across scripts.

--- _REFRESH_HIGH_MLF_ONLY_CURRENT.sh
+++ _REFRESH_HIGH_MLF_ONLY_CURRENT.sh
@@ -11,13 +11,13 @@ export JGTPY_DATA
 export JGTPY_DATA_FULL
 
 
-#(cd $JGTPY_DATA/cds;for f in *.csv;do droxul upload $f /dist/data/current/cds;done)
+
 for t in W1 M1 D1 H4 ;do 
   for i in EUR/USD USD/CAD SPX500 AUD/USD AUD/CAD GBP/USD;do 
 	    for p in mfi mz zonesq;do 
-		    ttfcli -i "$i" -t "$t" -pn "$p" --full -old && \
-	    	mlfcli -i "$i" -t "$t" -pn "$p" --full -old && \
-	    jgtmlcli -i "$i" -t "$t" -pn "$p" --full -old 
+		    ttfcli -i "$i" -t "$t" -pn "$p" -old && \
+	    	mlfcli -i "$i" -t "$t" -pn "$p"  -old
+	   
     done
   done
 done
